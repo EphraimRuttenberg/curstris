373cf6d17766b250e7640abb62c393e7b724f9d9
Ephraim Ruttenberg <ephraim408@gmail.com>
2020-07-23 10:10:55 -0400

    Improved controls handling, and fixed rotation for some pieces

--- game.py
+++ game.py
@@ -53,29 +53,35 @@ class Game():
         self.board.deinit_scr()
 
     def check_keys(self):
-        c = self.board.screen.getch()
+        keys_pressed = []
+        while 1:
+            try:
+                keys_pressed.append(self.board.screen.getkey())
+            except:
+                break
 
-        if c == curses.KEY_LEFT:
-            self.board.move_x(-1)
+        for c in keys_pressed:
+            if c == "KEY_LEFT":
+                self.board.move_x(-1)
 
-        if c == curses.KEY_RIGHT:
-            self.board.move_x(1)
+            if c == "KEY_RIGHT":
+                self.board.move_x(1)
 
-        if c == curses.KEY_DOWN:
-            self.board.gravity()
-            # Reset frame count so gravity does not work unpredictably with soft drop
-            self.frame_count = 1
+            if c == "KEY_DOWN":
+                self.board.gravity()
+                # Reset frame count so gravity does not work unpredictably with soft drop
+                self.frame_count = 1
 
-        if c == curses.KEY_UP:
-            lowest_y = self.board.get_lowest_valid_y()
-            self.board.active_piece.y = lowest_y
-            self.board.solidify()
-            self.frame_count = 1
-            self.board.active_piece = piece.Piece(self.retrieve_piece())
-        
-        if c == ord("a"):
-            self.board.rotate_piece(board.rotate_left, -1)
+            if c == "KEY_UP":
+                lowest_y = self.board.get_lowest_valid_y()
+                self.board.active_piece.y = lowest_y
+                self.board.solidify()
+                self.frame_count = 1
+                self.board.active_piece = piece.Piece(self.retrieve_piece())
+            
+            if c == "a":
+                self.board.rotate_piece(board.rotate_left, -1)
 
-        if c == ord("s"):
-            self.board.rotate_piece(board.rotate_right, 1)
+            if c == "s":
+                self.board.rotate_piece(board.rotate_right, 1)
 
